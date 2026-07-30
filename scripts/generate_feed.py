642ed658ed13e4544427253172b80c1040629a27
ufuksimsekk <simsekufuk4@gmail.com>
2026-07-30 11:59:24 +0300

    Refactor IP fetching with pagination and limits
    Refactor the IP fetching logic to use parameters for pagination and limit the number of results per request.

--- scripts/generate_feed.py
+++ scripts/generate_feed.py
@@ -2,43 +2,17 @@ import requests
 
 BASE_URL = "https://siberguvenlik.gov.tr/api"
 
-all_ips = []
+params = {
+    "type": "ip",
+    "page": 0,
+    "per-page": 3
+}
 
-page = 0
+r = requests.get(BASE_URL, params=params, timeout=60)
 
-while True:
+print("Status:", r.status_code)
+print("Content-Type:", r.headers.get("Content-Type"))
+print("Response:")
+print(r.text)
 
-    params = {
-        "type": "ip",
-        "page": page,
-        "per-page": 9999
-    }
-
-    r = requests.get(BASE_URL, params=params, timeout=60)
-    r.raise_for_status()
-
-    data = r.json()
-
-    models = data.get("models", [])
-
-    if len(models) == 0:
-        break
-
-    for item in models:
-
-        ip = item.get("url")
-
-        if ip:
-            all_ips.append(ip)
-
-    print(f"Page {page} : {len(models)} kayıt")
-
-    page += 1
-
-with open("feeds/ip.txt","w") as f:
-
-    for ip in sorted(set(all_ips)):
-
-        f.write(ip + "\n")
-
-print(f"Toplam {len(all_ips)} IP")
+r.raise_for_status()
